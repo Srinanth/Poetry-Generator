69aedc8fe2a4257a24921987a9e75373dadbb7b2
Srinanth <srinanthmv845@gmail.com>
2025-03-18 14:31:43 +0530

    Update app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 
 //middleware
-app.use(cors({origin:'http://localhost:5173',methods:["POST","GET"],credentials:true}));
+app.use(cors({origin:'http://localhost:5173',credentials:true}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE));
 
