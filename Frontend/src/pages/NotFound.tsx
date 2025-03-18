2b626f507d2c5d6c671b47dbd71d235d47e231dc
Srinanth <srinanthmv845@gmail.com>
2025-03-18 10:25:25 +0530

    Update NotFound.tsx

--- Frontend/src/pages/NotFound.tsx
+++ Frontend/src/pages/NotFound.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-//later
-const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+// import React from 'react'
+// //later
+// const NotFound = () => {
+//   return (
+//     <div>NotFound</div>
+//   )
+// }
 
-export default NotFound
+// export default NotFound
