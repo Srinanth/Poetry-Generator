357563375801525d7d9f9871aec5514f9e5a1b88
Srinanth <srinanthmv845@gmail.com>
2025-07-22 11:32:32 +0530

    domain check

--- Backend/src/controllers/user-controller.ts
+++ Backend/src/controllers/user-controller.ts
@@ -27,7 +27,7 @@ export const userSignup = async (req: Request,res: Response) => {
     await user.save();
 // creating tokens and storing cookies,same for login
     res.clearCookie(COOKIE_NAME, {
-      domain: "localhost",
+      domain: 'https://poetry-ai-theta.vercel.app',
       httpOnly: true,
       signed: true,
       path: "/",
@@ -38,7 +38,7 @@ export const userSignup = async (req: Request,res: Response) => {
     expires.setDate(expires.getDate() + 7);
     res.cookie(COOKIE_NAME, token, {
       path: "/",
-      domain: "localhost",
+      domain: 'https://poetry-ai-theta.vercel.app',
       expires,
       httpOnly: true,
       signed: true,
@@ -65,7 +65,7 @@ export const userlogin = async (req: Request,res: Response) => {
     }
     
     res.clearCookie(COOKIE_NAME, {
-      domain: "localhost",
+      domain: 'https://poetry-ai-theta.vercel.app',
       httpOnly: true,
       signed: true,
       path: "/",
@@ -76,7 +76,7 @@ export const userlogin = async (req: Request,res: Response) => {
     expires.setDate(expires.getDate() + 7);
     res.cookie(COOKIE_NAME, token, {
       path: "/",
-      domain: "localhost",
+      domain: 'https://poetry-ai-theta.vercel.app',
       expires,
       httpOnly: true,
       signed: true,
